5957535fff5722a37f0ef5533c918f15797cf392
cbares <38895035+cbares@users.noreply.github.com>
2025-05-11 23:20:26 +0200

    setup launch file

---
+++ send_udp/launch/gps_sbus_launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    sbus_port = DeclareLaunchArgument(
+            'port',
+            default_value='/dev/ttyAMA5',
+            description='Serial port for SBUS communication'
+        )
+    sbus_baud = DeclareLaunchArgument(
+            'baudrate',
+            default_value='100000',
+            description='Baudrate for SBUS communication'
+        )
+    
+    sbus_node = Node(
+            package='sbus_serial',
+            executable='sbus_serial_node',
+            name='sbus_serial_node',
+            parameters=[{
+                'port': LaunchConfiguration('port'),
+                'baudrate': LaunchConfiguration('baudrate')
+            }]
+        )
+    
+    sbus_cmd_vel_node = Node(
+            package='sbus_serial',
+            executable='sbus_serial_node',
+            name='sbus_serial_node',
+            parameters=[{
+            }]
+        )
+
+    return LaunchDescription([
+        sbus_port,
+        sbus_baud,
+        sbus_node, 
+        sbus_cmd_vel_node   
+    ])
