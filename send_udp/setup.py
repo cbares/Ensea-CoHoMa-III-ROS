90ddb6ce3080c9e55ec48f5d8d2d04e98d61c433
cbares <38895035+cbares@users.noreply.github.com>
2025-05-12 00:18:59 +0200

    change setup.py

--- send_udp/setup.py
+++ send_udp/setup.py
@@ -10,13 +10,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='theseus',
-    maintainer_email='theseus@todo.todo',
+    maintainer_email='38895035+cbares@users.noreply.github.com',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
