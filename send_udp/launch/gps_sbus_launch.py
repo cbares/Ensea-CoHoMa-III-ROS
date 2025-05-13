7a105062591c38ad3dfffaeeb43362a31334306d
cbares <38895035+cbares@users.noreply.github.com>
2025-05-14 00:56:39 +0200

    setup send_udp launch

--- send_udp/launch/gps_sbus_launch.py
+++ send_udp/launch/gps_sbus_launch.py
@@ -8,37 +8,46 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
 
     sbus_port = DeclareLaunchArgument(
-            'port',
-            default_value='/dev/ttyAMA5',
-            description='Serial port for SBUS communication'
-        )
+        'port',
+        default_value='/dev/ttyAMA5',
+        description='Serial port for SBUS communication'
+    )
     sbus_baud = DeclareLaunchArgument(
-            'baudrate',
-            default_value='100000',
-            description='Baudrate for SBUS communication'
-        )
+        'baudrate',
+        default_value='100000',
+        description='Baudrate for SBUS communication'
+    )
     
     sbus_node = Node(
-            package='sbus_serial',
-            executable='sbus_serial_node',
-            name='sbus_serial_node',
-            parameters=[{
-                'port': LaunchConfiguration('port'),
-                'baudrate': LaunchConfiguration('baudrate')
-            }]
-        )
+        package='sbus_serial',
+        executable='sbus_serial_node',
+        name='sbus_serial_node',
+        parameters=[{
+            'port': LaunchConfiguration('port'),
+            'baudrate': LaunchConfiguration('baudrate')
+        }]
+    )
     
     sbus_cmd_vel_node = Node(
-            package='sbus_serial',
-            executable='sbus_cmd_vel_node',
-            name='sbus_cmd_vel_node',
-            parameters=[{
-            }]
-        )
+        package='sbus_serial',
+        executable='sbus_cmd_vel_node',
+        name='sbus_cmd_vel_node',
+        parameters=[{
+        }]
+    )
+
+    send_udp = Node(
+        package='send_udp',
+        executable='send_udp',
+        name='send_udp',
+        parameters=[{
+        }]
+    )
 
     return LaunchDescription([
         sbus_port,
         sbus_baud,
         sbus_node, 
-        sbus_cmd_vel_node   
+        sbus_cmd_vel_node,
+        send_udp,   
     ])
--- send_udp/send_udp/send_udp.py
+++ send_udp/send_udp/send_udp.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
+import os
 import socket
 import json
+import requests
 
 import rospy
 from sensor_msgs.msg import NavSatFix
 
-NAME = "2_TER_PETIT-POUCET"
-UDP_IP = "192.168.2.213" #debug
-UDP_PORT = 32001
+
+# Load environment variables from the current environment only
+NAME = os.environ.get("NAME", "3_TER_ARIANE")
+UDP_IP = os.environ.get("UDP_IP", "192.168.2.2")
+UDP_PORT = int(os.environ.get("UDP_PORT", 32001))
+IHM_IP = os.environ.get("IHM_IP", "192.168.2.2")
+IHM_PORT = os.environ.get("IHM_PORT", "8008")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def callback(data):
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    print(data)    
+def setup_data_udp(data):
     dt = {}
     dt["uav id"] = NAME	
     dt["uav longitude"] = data.longitude
@@ -22,13 +26,37 @@ def callback(data):
     dt["uav speed"] = 0
     dt["uav heading"] = 0
     jdata = json.dumps(dt)
-    Message = bytes(jdata)
     
-	
+    return jdata
+
+def setup_data_rest(data):
+    dt = {}
+    dt["name"] = NAME
+    dt["longitude"] = data.longitude
+    dt["latitude"] = data.latitude
+    #dt["altitude"] = data.altitude
+    #dt["speed"] = 0
+    #dt[" heading"] = 0
+    jdata = json.dumps(dt)
+    
+    return jdata
+
+def callback_udp(data):
+    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    print(data)
+    jdata = setup_data_udp(data)
+    Message = bytes(jdata)
+
     sock.sendto(Message, (UDP_IP, UDP_PORT))
 
     rospy.loginfo(Message)
-    
+
+def callback_rest_api(data):
+    jdata = setup_data_rest(data)
+    Message = bytes(jdata)
+
+    requests.post(f"http://{IHM_IP}:{IHM_PORT}/api/satellite/update", json=Message)
+
 def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -38,7 +66,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("/fix", NavSatFix, callback)
+    rospy.Subscriber("/fix", NavSatFix, callback_udp)
 
     while not rospy.core.is_shutdown():
         rospy.rostime.wallsleep(0.5)
